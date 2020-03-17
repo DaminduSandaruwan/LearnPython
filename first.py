09008acd61a7f13f51b1ec4a0e79506189905988
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-17 12:33:57 +0530

    Getting Started, Variables

--- first.py
+++ first.py
@@ -1,1 +1,47 @@
 print("Hello World");
+print("Damindu Sandaruwan");
+
+print(9-8);
+print(4*6);
+print(8/4);
+print(5/2);
+print(5//2); #interger division. when we want only int number use // 
+print(8+9-10);
+print(5%2);
+
+name = 'Damindu';
+print(name);
+
+laptop = "Damindu's Laptop"; #some times we cant use single quote
+print(laptop);
+
+laptop='Damindu\'s Laptop';
+print(laptop);
+
+print(r'c:\docs\damindu\name'); #row string r
+
+
+
+#Variables
+
+x=2; #container - Assignment
+print(x+3);
+
+y=5
+print(x+y);
+
+print(name + ' Development');
+
+print(name[0]); #print index 0 of name
+print(name[1]);
+print(name[-1]);#print last 
+
+print(name[0:2]); #Da
+print(name[1:4]); #ami
+print(name[1:]); #amindu
+print(name[:4]); #Dami
+print(name[3:10]); #indu
+
+#String in python cannot change(does not support item assignment) - immutable
+print(len(name)); #7
+
