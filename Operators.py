046db7ce82815603f828ef4107b2682b664906ed
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-17 22:48:10 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,52 @@
+#Arithamatic Operator,Assignment Operator,Relational Operator,Logical Operator, Unary Operator
+
+#Arithamatic Operator
+x= 20
+y = 10
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+
+ 
+x+=2 #x = x + 2
+x-=2 #x = x - 2 
+
+a,b = 25,65
+print(a) #25
+print(b) #65
+
+#Unary Operator
+a  = 56
+print(-a) #-56
+
+a = -a
+print(a) #-56
+
+#Relational Operator
+
+a = 5
+b = 6
+
+print(a<b) #True 
+print(a>b) #False
+print(a==b) #False
+print(a<=b) #True
+print(a>=b) #False
+print(a!=b) #True
+
+
+#Logical Operator
+
+a = 5
+b = 4
+
+print(a < 8 and b < 5) #True
+print(a < 8 and b < 2) #False
+
+print(a < 8 or b < 5) #True
+print(a < 8 or b < 2) #True
+
+x = True
+print(not x) #False
+
