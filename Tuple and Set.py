4163f01b1c7e79addb5f6f6fe133ff2906b4f3cb
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-17 16:02:57 +0530

    Tuple and Set

---
+++ Tuple and Set.py
@@ -0,0 +1,17 @@
+#Tuple (same as list but immutible(cannot change))
+
+tup =(21,65,35,26,45)
+print(tup) #(21, 65, 35, 26, 45)
+
+print(tup[1]) #65
+
+#SET(use {} ) 
+
+# *indexing not support
+#  *not support duplicate value 
+s = {22,25,69,56,45} 
+print(s) #{69, 45, 22, 56, 25}
+
+s={56,65,48,36,58,49,58,58,58,75}
+print(s) #{65, 36, 75, 48, 49, 56, 58}
+
--- first.py
+++ first.py
@@ -1,39 +1,39 @@
-print("Hello World");
-print("Damindu Sandaruwan");
+print("Hello World")
+print("Damindu Sandaruwan")
 
-print(9-8);
-print(4*6);
-print(8/4);
-print(5/2);
+print(9-8)
+print(4*6)
+print(8/4)
+print(5/2)
 print(5//2); #interger division. when we want only int number use // 
-print(8+9-10);
-print(5%2);
+print(8+9-10)
+print(5%2)
 
-name = 'Damindu';
-print(name);
+name = 'Damindu'
+print(name)
 
 laptop = "Damindu's Laptop"; #some times we cant use single quote
 print(laptop);
 
-laptop='Damindu\'s Laptop';
-print(laptop);
+laptop='Damindu\'s Laptop'
+print(laptop)
 
-print(r'c:\docs\damindu\name'); #row string r
+print(r'c:\docs\damindu\name') #row string r
 
 
 
 #Variables
 
 x=2; #container - Assignment
-print(x+3);
+print(x+3)
 
 y=5
-print(x+y);
+print(x+y)
 
-print(name + ' Development');
+print(name + ' Development')
 
 print(name[0]); #print index 0 of name
-print(name[1]);
+print(name[1])
 print(name[-1]);#print last 
 
 print(name[0:2]); #Da
