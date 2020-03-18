325b941741c3196fd2a6026513b2e40bee49fe02
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 19:02:31 +0530

    User Input and cmd Inputs

---
+++ User Input.py
@@ -0,0 +1,18 @@
+#User Input
+x = int(input("Enter First Number : ")) #must convert into int for calculation
+y = int(input("Enter Secound Number : "))
+z = x+y
+print(z)
+
+#getting char
+ch = input('Enter a char : ')
+print(ch)
+
+#but user insert string
+ch=input('Enter Char : ')[0]
+print(ch) # in this case when user input string but only get first char
+
+#Eval function
+result = eval(input('Enter a char : '))
+print(result) # in this case user input 2+5+6 like that we can get result of that as 13
+
---
+++ cmdInput.py
@@ -0,0 +1,7 @@
+import sys
+
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+z=x+y
+print(z)
+#when running on cmd prompt we can give input
