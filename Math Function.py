fed052df3a7ccd1d8c07c89cae9348023e8b86cd
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 17:35:55 +0530

    Math Function

---
+++ Math Function.py
@@ -0,0 +1,27 @@
+#Math Function
+
+import math
+
+print(math.sqrt(25)) #5.0
+print(math.sqrt(15)) #3.872983346207417
+
+print(math.floor(2.9)) #2 floor is 2
+print(math.ceil(2.9)) #3 
+print(math.floor(2.1)) #2
+
+print(math.pow(3,2)) #9.0 3*3
+print(math.pi) #3.141592653589793
+print(math.e) #2.718281828459045
+
+import math as m
+print(m.sqrt(25)) #5.0
+
+from math import sqrt,pow
+print(sqrt(25)) #5.0
+print(pow(2,5)) #32.0
+
+
+
+
+
+
