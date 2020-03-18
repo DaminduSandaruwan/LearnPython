c2431261dc1c21176c59689ad4f1227bcfffc27f
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 22:22:25 +0530

    While Loops

---
+++ While Loops.py
@@ -0,0 +1,25 @@
+#while
+
+i = 1
+while i<=5:
+    print("Hello !")
+    i=i+1
+
+i = 5
+j=1
+while i>=1:
+    print("Hello Damindu !")
+    while j<=4:
+        print("Develop")
+        j=j+1
+    i=i-1
+
+i = 5
+while i>=1:
+    print("Hello Damindu ! " , end = "")
+    j=1
+    while j<=4:
+        print("Develop " , end ="")
+        j=j+1
+    i=i-1
+    print()
