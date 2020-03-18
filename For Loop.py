bbc6e4f655c7b6a3cb19e1be6d27bf8f09c1ecb5
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 22:59:14 +0530

    For Loop

---
+++ For Loop.py
@@ -0,0 +1,29 @@
+#For Loop
+
+x= ['Damindu',62,5.6]
+
+for i in x:
+    print(i)
+
+
+y = 'DAMINDU'
+for i in y:
+    print(i) #D   A   M   I  N  D  U
+
+
+for i in range(10):
+    print(i) # print 0 to 9
+
+for i in range(11,21,1):
+    print(i) #print 11 to 20
+
+for i in range(20,10,-1):
+    print(i)  #print 20 to 11 #
+
+for i in range(1,21):
+    if(i%5 != 0):
+        print(i) # print 11,12,13,14,16,17,18,19
+
+
+
+
