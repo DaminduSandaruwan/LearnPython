4a5b5695dc0219c4955ec11f0b9434fbec943124
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 14:49:48 +0530

    Number System Conversation

---
+++ Number System Conversation.py
@@ -0,0 +1,23 @@
+#Decimal to Binary
+print(bin(25)) #0b11001 0b represent binary format
+
+#Binary to Decimal
+print(0b0101) #5
+
+#Decimal to Octal
+print(oct(25)) #0o31
+
+#Octal to Decimal
+print(0o45) #37
+
+#Decimal to Hexadecimal
+print(hex(25)) #0x19
+
+#Hexadecimal to Binary
+print(0xf152) #61778
+
+
+
+
+
+
