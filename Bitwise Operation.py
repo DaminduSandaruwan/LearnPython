9f3a80f626042c148eb44bbfeae6ac4adb360e6c
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-18 16:41:35 +0530

    BitWise Operation

---
+++ Bitwise Operation.py
@@ -0,0 +1,20 @@
+#Complement(~) AND(&) OR(|) XOR(^) LeftShift(<<) RightShift(>>)
+
+#Complement(~)
+print(~12) #-13 get by 2's complement
+print(~14) #-15
+
+#AND(&)
+print(12 & 13) #12 convert into binary and do AND operation
+
+#OR(|)
+print(12 | 13) #13 convert into binary and do OR operation
+
+#XOR(^)
+print(12 ^ 13) #1 convert into binary and do XOR Operation
+
+#LeftShift(<<)
+print(10<<2) #40 shift 2 bit 1010. ---> 101000.
+
+#RightShift(>>)
+print(10>>2) #2 shift 2 bit 1010. --->10.
