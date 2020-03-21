b1f92d54894503e63418d313d2f8d7f4533f85d3
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-21 12:25:21 +0530

    Modules

---
+++ Modules/Calc.py
@@ -0,0 +1,14 @@
+
+
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
---
+++ Modules/Demo.py
@@ -0,0 +1,10 @@
+#Modules
+
+from Calc import *
+#import Calc
+
+a = 9
+b = 7
+
+result = add(a,b) #Calc.add
+print(result)
